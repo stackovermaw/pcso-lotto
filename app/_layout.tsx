9a5ba847bba4be084169e9d850e2a3cb805b280a
stackovermaw <johncarlo.camara1@gmail.com>
2026-01-07 22:04:31 +0800

    feat: init header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,40 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import { Stack } from "expo-router";
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
-
+// TODO: setup themed text just like from app example.
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack
+      screenOptions={{
+        header: (props) => (
+          <SafeAreaView style={{ paddingHorizontal: 10 }}>
+            <View
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                gap: 10,
+                alignItems: "center",
+              }}
+            >
+              <FontAwesome name="bullseye" size={24} />
+              <View style={{ display: "flex", flexDirection: "column" }}>
+                <Text {...props} style={{ fontSize: 25 }}>
+                  Lotto Results
+                </Text>
+                <Text {...props}>Wednesday, January 7, 2026</Text>
+              </View>
+              <AntDesign
+                name="api"
+                size={24}
+                color="red"
+                style={{ marginLeft: "auto" }}
+              />
+            </View>
+          </SafeAreaView>
+        ),
+      }}
+    />
   );
 }
---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Sample</Text>
+    </View>
+  );
+}
+
+export const options = {
+  title: "Home",
+};
