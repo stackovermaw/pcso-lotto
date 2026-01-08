d18f179b393562b9b533c5e74e01e4024226bc42
stackovermaw <johncarlo.camara1@gmail.com>
2026-01-08 21:23:19 +0800

    refactor: use components and stylesheets

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,42 +1,69 @@
 import { AntDesign, FontAwesome } from "@expo/vector-icons";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
 import { Stack } from "expo-router";
-import { View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, useColorScheme } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 import { ThemedText } from "@/components/ui/themed-text";
+import { ThemedView } from "@/components/ui/themed-view";
+import { useThemeColor } from "@/hooks/use-theme-color";
 
 export default function RootLayout() {
-  return (
-    <Stack
-      screenOptions={{
-        header: (props) => (
-          <SafeAreaView style={{ paddingHorizontal: 10 }}>
-            <View
-              style={{
-                display: "flex",
-                flexDirection: "row",
-                gap: 10,
-                alignItems: "center",
-              }}
-            >
-              <FontAwesome name="bullseye" size={30} />
-              <View style={{ display: "flex", flexDirection: "column" }}>
-                <ThemedText type="title">Lotto Results</ThemedText>
+  const theme = useColorScheme();
+  const backgroundColor = useThemeColor({}, "background");
 
-                <ThemedText type="default">
-                  Wednesday, January 7, 2026
-                </ThemedText>
-              </View>
-              <AntDesign
-                name="api"
-                size={30}
-                color="red"
-                style={{ marginLeft: "auto" }}
-              />
-            </View>
-          </SafeAreaView>
-        ),
-      }}
-    />
+  const Header = () => (
+    <SafeAreaView style={[styles.safeArea, { backgroundColor }]}>
+      <ThemedView style={styles.row}>
+        <FontAwesome name="bullseye" size={30} style={styles.iconLeft} />
+        <ThemedView style={styles.column}>
+          <ThemedText type="title">Lotto Results</ThemedText>
+          <ThemedText type="default">
+            {new Date().toLocaleDateString("en-US", {
+              weekday: "long",
+              month: "long",
+              day: "numeric",
+              year: "numeric",
+            })}
+          </ThemedText>
+        </ThemedView>
+        <AntDesign name="api" size={30} color="red" style={styles.iconRight} />
+      </ThemedView>
+    </SafeAreaView>
+  );
+
+  return (
+    <ThemeProvider value={theme === "dark" ? DarkTheme : DefaultTheme}>
+      <Stack
+        screenOptions={{
+          header: () => <Header />,
+        }}
+      />
+      <StatusBar style="auto" />
+    </ThemeProvider>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    paddingHorizontal: 10,
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  column: {
+    flexDirection: "column",
+  },
+  iconLeft: {
+    marginRight: 10,
+  },
+  iconRight: {
+    marginLeft: "auto",
+  },
+});
